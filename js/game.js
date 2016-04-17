16d62d95b2b87b6fa199e55e7fe329b182f1f158
terentz <terentz.hexadec@gmail.com>
2016-04-17 11:53:58 +1000

    game.js started. Graphing library added

---
+++ js/game.js
@@ -0,0 +1,26 @@
+/*
+ * 
+ */
+
+/* GLOBALS */
+
+var started = false;
+
+var map 
+
+
+/* TOP LEVEL FUNCTIONS */
+
+
+function placeNext ( ) {
+    var target = $(event.currentTarget);
+    
+    if ( started ) {
+        // Not first move
+    }
+    else {
+        // First move
+    }
+}
+
+
