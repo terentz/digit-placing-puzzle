9ee49f53713e7c49dd82a1e7e6dbf72b1f0c02a0
terentz <terentz.hexadec@gmail.com>
2016-04-17 18:16:43 +1000

    Gameplay complete

--- js/game.js
+++ js/game.js
@@ -5,17 +5,98 @@
 
 
 /* TOP LEVEL FUNCTIONS */
+var test;
 
-
-function placeNext ( ) {
+function move ( ) {
     var target = $(event.currentTarget);
+    var node = target.attr('id');
     
-    if ( started ) {
-        // Not first move
-    }
-    else {
-        // First move
+    // If take back..
+    if ( node === currNode ) {
+        console.log('take back');
+        moveBack(node);
+    } else {
+        // Otherwise, move forward..
+        if ( validNodes.indexOf(node) > -1 ) {
+            moveFwd( node );
+        }
     }
 }
 
+function moveFwd( id ) {
+    // Set prevNode..
+    if ( !started ) {
+        started = true;
+        prevNode = '';
+    } else {
+        prevNode = currNode;
+    }
+    
+    // Update current node..
+    currNode = id;                                      // Update current node
+    usedNodes.push(id);                                 // Add current to usedNodes
+    $('#txt_'+id).text( (++current).toString() );       // Add a number to the new current node 
 
+    // Set the nodes that are now valid..
+    setValid(id);
+}
+
+function moveBack ( id ) {
+    --current;
+    // Blank out current square..
+    $('#txt_'+id).text('');
+    $('#'+id).attr('fill', 'white');
+    
+    // Pop from usedNodes, and update main node types..
+    nextNode = usedNodes.pop();
+    if ( usedNodes.length > 0 ) {
+        currNode = usedNodes[usedNodes.length-1];
+    } else { 
+        currNode = null;
+        started = false;
+    }
+    if ( usedNodes.length > 1 ) {
+        prevNode = usedNodes[usedNodes.length-2];
+    } else {
+        prevNode = null;
+    }
+    
+    // Set valid nodes..
+    setValid( started ? currNode : '0' );
+}
+
+
+
+/* GAME UTILITIES */
+
+
+
+function setValid(id) {
+    console.log('in setValid()');
+    if ( !started ) {
+        // Set valid nodes (pre-game)
+        for ( node in NODES ) {
+            id = NODES[node];
+            validNodes.push( id );
+            $('#'+id).addClass('valid');
+        }
+    } else {
+        // Game started...
+        $('circle').removeClass('valid');
+        validNodes = null;
+        validNodes = new Array();
+        for ( node in NODES ) {
+            var other = NODES[node];
+            // If other is not in usedNodes and other is not adjacent to id... 
+            if ( usedNodes.indexOf( other ) < 0 && adjacent( id, other ) === false ) {
+                // Node is valid...
+                validNodes.push(other);
+                $('#'+other).addClass('valid');
+            }
+        }
+    }
+}
+
+function adjacent( id1, id2 ) {
+    return GRAPH[ NODES.indexOf(id1) ][ NODES.indexOf(id2) ];
+}
--- js/globals.js
+++ js/globals.js
@@ -3,6 +3,7 @@
 /* GLOBALS */
 
 NODES = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+
 //   A B C D E F G H
 // A - 1 1 1 0 0 0 0
 // B 1 - 1 0 1 1 0 0
@@ -12,7 +13,7 @@ NODES = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 // F 0 1 1 1 1 - 1 1
 // G 0 0 1 1 0 1 - 1
 // H 0 0 0 0 1 1 1 -
-//const GRAPH = [
+
 GRAPH = [
     [null,  true,   true,   true,   false,  false,  false,  false],
     [true,  null,   true,   false,  true,   true,   false,  false],
@@ -24,45 +25,22 @@ GRAPH = [
     [false, false,  false,  false,  true,   true,   true,   null]
 ];
 
-var started = false;
-var usedNodes = new Array();
+COLOURS = {
+    used : 'white',
+    valid : 'lightgreen',
+    current : 'green'
+};
+
+var started = false,
+    current = 0;
+var usedNodes = new Array(),
+    validNodes = new Array();
+var prevNode = null,
+    currNode = null,
+    nextNode = null;
 
 
 
 
 
 
-
-/* graph.js code */
-//var graph = new Graph();
-//var nodeA = graph.addNode('A'),
-//    nodeB = graph.addNode('B'),
-//    nodeC = graph.addNode('C'),
-//    nodeD = graph.addNode('D'),
-//    nodeE = graph.addNode('E'),
-//    nodeF = graph.addNode('F'),
-//    nodeG = graph.addNode('G'),
-//    nodeH = graph.addNode('H');
-///* A to first row */
-//nodeA.addEdge(nodeB);
-//nodeA.addEdge(nodeC);
-//nodeA.addEdge(nodeD);
-///* first row horizontals */
-//nodeB.addEdge(nodeC);
-//nodeC.addEdge(nodeD);
-///* inter-row verticals */
-//nodeB.addEdge(nodeE);
-//nodeC.addEdge(nodeF);
-//nodeD.addEdge(nodeG);
-///* inter-row diagonals */
-//nodeB.addEdge(nodeF);
-//nodeC.addEdge(nodeE);
-//nodeC.addEdge(nodeG);
-//nodeD.addEdge(nodeF);
-///* second row horizontals */
-//nodeE.addEdge(nodeF);
-//nodeF.addEdge(nodeG);
-///* second row to H */
-//nodeE.addEdge(nodeH);
-//nodeF.addEdge(nodeH);
-//nodeG.addEdge(nodeH);
